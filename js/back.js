5e132e4d77951e104a629b76bb59abcbaa9e4de7
godtears <169066251+soulcrying@users.noreply.github.com>
2024-10-19 14:35:13 +0800

    Update back.js

--- js/back.js
+++ js/back.js
@@ -1,10 +1,10 @@
 <script language="javascript">
-　　 image = new Array(4); //定义image为图片数量的数组
-　　 image [0] = './img/1.jpg' //背景图象的路径
-　　 image [1] = './img/2.jpg'
-　　 image [2] = './img/3.jpg'
-　　 image [3] = './img/4.jpg'
-　　 image [4] = './img/5.jpg'
+　　 image = new Array(4); 
+　　 image [0] = './imgs/1.jpg' 
+　　 image [1] = './imgs/2.jpg'
+　　 image [2] = './imgs/3.jpg'
+　　 image [3] = './imgs/4.jpg'
+　　 image [4] = './imgs/5.jpg'
 　　 number = Math.floor(Math.random() * image.length);
     var obj = document.getElementById("top-slider");
     obj.style.cssText ="background:url("+image[number]+");background-position: 0 0;    background-repeat: no-repeat;    background-attachment: fixed; margin: 0 auto; background-size: cover;"; 
